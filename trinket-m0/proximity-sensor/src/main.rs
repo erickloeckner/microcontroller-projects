f1c8ab49cfa2e9dbababf9b81eedb0c4d741c6ab
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2022-02-12 04:21:37 -0500

    Update main.rs

--- trinket-m0/proximity-sensor/src/main.rs
+++ trinket-m0/proximity-sensor/src/main.rs
@@ -1,153 +1,124 @@
 #![no_std]
 #![no_main]
 
-use panic_halt as _;
-use trinket_m0 as hal;
+mod buffer;
+use crate::buffer::RingBuffer;
+mod sensor;
+use crate::sensor::DistanceSensor;
 
+use core::cell::Cell;
+
+use bsp::hal;
+use panic_halt as _;
+use trinket_m0 as bsp;
+
+use bsp::entry;
+use cortex_m::interrupt as cortex_interrupt;
+use cortex_m::interrupt::Mutex;
+use cortex_m::peripheral::NVIC;
 use hal::clock::GenericClockController;
 use hal::delay::Delay;
-use hal::entry;
-use hal::pac::{CorePeripherals, Peripherals};
+use hal::pac::{interrupt, CorePeripherals, Peripherals, TC3};
 use hal::prelude::*;
-use atsamd_hal::gpio::Pin;
-use atsamd_hal::gpio::v2::pin::{Input, Output, PinId, PullDown, PushPull};
+use hal::timer::TimerCounter;
 
-struct RingBuffer8 {
-    buffer: [u16; 8],
-    pos: usize,
-}
-
-impl RingBuffer8 {
-    fn new() -> RingBuffer8 {
-        RingBuffer8 {
-            buffer: [0; 8],
-            pos: 0,
-        }
-    }
-    
-    fn push(&mut self, value: u16) {
-        self.buffer[self.pos] = value;
-        self.pos = (self.pos + 1) % self.buffer.len();
-    }
-    
-    fn mean(&mut self) -> u16 {
-        let sum: u32 = self.buffer
-            .iter()
-            .fold(0, |s, &i| s + (i as u32));
-        (sum / (self.buffer.len() as u32)) as u16
-        
-    }
-}
-
-struct DistanceSensor<T: PinId, E: PinId> {
-    trig: Pin<T, Output<PushPull>>,
-    echo: Pin<E, Input<PullDown>>,
-    wait_us: u16,
-    pulse_us: u16,
-    timeout: u16,
-}
-
-impl<T: PinId, E: PinId> DistanceSensor<T, E> {
-    fn get_value(&mut self, delay: &mut Delay) -> u16 {
-        self.wait_us = 0;
-        self.pulse_us = 0;
-        
-        self.trig.set_high().unwrap();
-        delay.delay_us(100u8);
-        self.trig.set_low().unwrap();
-        
-        while self.echo.is_low().unwrap() {
-            delay.delay_us(1u8);
-            self.wait_us = self.wait_us.saturating_add(1);
-            
-            if self.wait_us >= self.timeout {
-                break;
-            }
-        }
-        
-        while self.echo.is_high().unwrap() {
-            delay.delay_us(1u8);
-            self.pulse_us = self.pulse_us.saturating_add(1);
-            
-            if self.pulse_us >= self.timeout {
-                break;
-            }
-        }
-        self.pulse_us
-    }
-}
+const LOOP_TIME: u32 = 100;    // number of milliseconds between sensor updates
+const THRESHOLD: u16 = 520;    // minimum value of sensor average reading before switch is activated
+const HOLD_TIME: u32 = 300000; // number of milliseconds to keep switch active after sensor is above threshold
+const BUFFER_SIZE: usize = 8;  // ring buffer size for storing sensor readings
 
 enum MainState {
     Off,
     On,
 }
 
+static MILLIS: Mutex<Cell<u32>> = Mutex::new(Cell::new(0));
+
 #[entry]
 fn main() -> ! {
     let mut peripherals = Peripherals::take().unwrap();
-    let core = CorePeripherals::take().unwrap();
+    let mut core = CorePeripherals::take().unwrap();
     let mut clocks = GenericClockController::with_internal_32kosc(
         peripherals.GCLK,
         &mut peripherals.PM,
         &mut peripherals.SYSCTRL,
         &mut peripherals.NVMCTRL,
     );
-    let mut pins = hal::Pins::new(peripherals.PORT);
-    let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
+    let gclk0 = clocks.gclk0();
+    let tc23 = &clocks.tcc2_tc3(&gclk0).unwrap();
+    unsafe {
+        core.NVIC.set_priority(interrupt::TC3, 1);
+        NVIC::unmask(interrupt::TC3);
+    }
+    let mut timer = TimerCounter::tc3_(tc23, peripherals.TC3, &mut peripherals.PM);
+    timer.start(1.ms());
+    timer.enable_interrupt();
+    let mut millis: u32 = 0;
     
-    let mut relay_pin = pins.d2.into_open_drain_output(&mut pins.port);
+    let mut pins = bsp::Pins::new(peripherals.PORT);
+    let mut red_led = pins.d13.into_push_pull_output(&mut pins.port);
+    
+    let mut relay_pin = pins.d2.into_push_pull_output(&mut pins.port);
     relay_pin.set_high().unwrap();
     
     let mut delay = Delay::new(core.SYST, &mut clocks);
     
-    let mut sensor1 = DistanceSensor {
-        trig: pins.d0.into_open_drain_output(&mut pins.port),
-        echo: pins.d1.into_pull_down_input(&mut pins.port),
-        wait_us: 0u16,
-        pulse_us: 0u16,
-        timeout: 65535u16,
-    };
+    let mut sensor1 = DistanceSensor::new(
+        pins.d0.into_push_pull_output(&mut pins.port).into(),
+        pins.d1.into_pull_down_input(&mut pins.port).into(),
+        65535u16,
+    );
     
-    let mut buf = RingBuffer8::new();
+    let mut buf = RingBuffer::<u16, BUFFER_SIZE>::new();
     
     let mut state = MainState::Off;
-    let hold_time: u16 = 60000;
-    let mut hold_current: u16 = 0;
-    let threshold = 520;
+    let mut last_update: u32 = 0;
+    let mut last_on: u32 = 0;
     
     loop {
-        match state {
-            MainState::Off => {
-                buf.push(sensor1.get_value(&mut delay));
-                
-                if buf.mean() < threshold {
-                    red_led.set_high().unwrap();
-                    relay_pin.set_low().unwrap();
-                    state = MainState::On;
-                }
-                else {
-                    delay.delay_ms(100u16);
-                }
-            }
+        cortex_interrupt::free(|cs| {
+            millis = MILLIS.borrow(cs).get();
+        });
+        
+        if millis.wrapping_sub(last_update) >= LOOP_TIME {
+            last_update = millis;
+            buf.push(sensor1.get_value(&mut delay));
             
-            MainState::On => {
-                buf.push(sensor1.get_value(&mut delay));
-                
-                if buf.mean() < threshold {
-                    hold_current = 0;
+            match state {
+                MainState::Off => {
+                    if buf.mean() < THRESHOLD {
+                        red_led.set_high().unwrap();
+                        relay_pin.set_low().unwrap();
+                        last_on = millis;
+                        state = MainState::On;
+                    }
                 }
-                else {
-                    hold_current += 100;
-                }
-                delay.delay_ms(100u16);
-                
-                if hold_current >= hold_time {
-                    hold_current = 0;
-                    red_led.set_low().unwrap();
-                    relay_pin.set_high().unwrap();
-                    state = MainState::Off;
+                MainState::On => {
+                    if buf.mean() < THRESHOLD {
+                        last_on = millis;
+                    }
+                    
+                    if millis.wrapping_sub(last_on) >= HOLD_TIME {
+                        red_led.set_low().unwrap();
+                        relay_pin.set_high().unwrap();
+                        state = MainState::Off;
+                    }
                 }
             }
         }
     }
+}
+
+#[interrupt]
+fn TC3() {
+    cortex_interrupt::free(|cs| MILLIS.borrow(cs).set(MILLIS.borrow(cs).get().wrapping_add(1)));
+    
+    unsafe {
+        TC3::ptr()
+            .as_ref()
+            .unwrap()
+            .count16()
+            .intflag
+            .modify(|_, w| w.ovf().set_bit());
+        }
 }
