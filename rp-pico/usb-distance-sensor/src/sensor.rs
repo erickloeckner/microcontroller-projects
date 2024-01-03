051dddc875ce58e1b42c26c65a084159edb4ca6e
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2024-01-03 15:26:23 -0500

    Update sensor.rs

--- rp-pico/usb-distance-sensor/src/sensor.rs
+++ rp-pico/usb-distance-sensor/src/sensor.rs
@@ -1,15 +1,14 @@
-use rp2040_hal::gpio::{Input, Output, Pin, PinId, PullDown, PushPull};
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use cortex_m::delay::Delay;
 
-pub struct DistanceSensor<T: PinId, E: PinId> {
-    trig: Pin<T, Output<PushPull>>,
-    echo: Pin<E, Input<PullDown>>,
+pub struct DistanceSensor <T: OutputPin, E: InputPin> {
+    trig: T,
+    echo: E,
     timeout: u16,
 }
 
-impl<T: PinId, E: PinId> DistanceSensor<T, E> {
-    pub fn new(trig: Pin<T, Output<PushPull>>, echo: Pin<E, Input<PullDown>>, timeout: u16) -> Self {
+impl<T: OutputPin, E: InputPin> DistanceSensor<T, E> {
+    pub fn new(trig: T, echo: E, timeout: u16) -> Self {
         DistanceSensor { trig: trig, echo: echo, timeout: timeout }
     }
     
@@ -17,11 +16,11 @@ impl<T: PinId, E: PinId> DistanceSensor<T, E> {
         let mut wait_us: u16 = 0;
         let mut pulse_us: u16 = 0;
         
-        self.trig.set_high().unwrap();
+        let _ = self.trig.set_high();
         delay.delay_us(100);
-        self.trig.set_low().unwrap();
+        let _ = self.trig.set_low();
         
-        while self.echo.is_low().unwrap() {
+        while self.echo.is_low().ok().is_some() {
             delay.delay_us(1);
             wait_us = wait_us.saturating_add(1);
             if wait_us >= self.timeout {
@@ -29,7 +28,7 @@ impl<T: PinId, E: PinId> DistanceSensor<T, E> {
             }
         }
         
-        while self.echo.is_high().unwrap() {
+        while self.echo.is_high().ok().is_some() {
             delay.delay_us(1);
             pulse_us = pulse_us.saturating_add(1);
             if pulse_us >= self.timeout {
